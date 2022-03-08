48458c10830833573d658aae203eae0c4eb3191b
Amit Vashist <amitvashist7@outlook.com>
2022-03-08 11:52:25 +0000

    12-Excerise

--- 11-Outputs/output.tf
+++ 11-Outputs/output.tf
@@ -1,7 +1,24 @@
+/*
 output "PIP" {
   value = azurerm_public_ip.examplepublicip
 }
+*/
+
 
 output "my_vm_public_ip" {
   value = azurerm_public_ip.examplepublicip.ip_address
+}
+
+
+output "RG" {
+  value = azurerm_resource_group.example_rg.name
+}
+
+
+output "Pvt_IP" {
+  value = azurerm_network_interface.example_rg.private_ip_address
+}
+
+output "Storage_Acc_Detials" {
+ value = azurerm_network_interface.example_rg.internal_domain_name_suffix
 }
